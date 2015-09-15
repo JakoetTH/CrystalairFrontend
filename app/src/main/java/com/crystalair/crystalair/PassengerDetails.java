83453a137c63814b6396cdf713438f8f2d20dcec
unknown <ThawhirJ@SDRSA08.SPATIAL.local>
2015-09-15 11:51:30 +0200

    commit from work

--- app/src/main/java/com/crystalair/crystalair/PassengerDetails.java
+++ app/src/main/java/com/crystalair/crystalair/PassengerDetails.java
@@ -1,12 +1,12 @@
 package com.crystalair.crystalair;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class PassengerDetails extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
